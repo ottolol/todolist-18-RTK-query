13f31a32966e72902ff3b043c74c2be8ac4e63a1
ottolol <alexsveshnik@gmail.com>
2025-06-04 11:19:06 +0300

    4. Сделали Code Splitting, добавили baseApi.ts

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,27 +1,12 @@
 // Во избежание ошибок импорт должен быть из `@reduxjs/toolkit/query/react`
-import { AUTH_TOKEN } from "@/common/constants"
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { DomainTodolist } from "../model/todolists-slice"
 import { Todolist } from "./todolistsApi.types"
 import { BaseResponse } from "@/common/types"
+import { baseApi } from "@/app/baseApi"
 
 // `createApi` - функция из `RTK Query`, позволяющая создать объект `API`
 // для взаимодействия с внешними `API` и управления состоянием приложения
-export const todolistsApi = createApi({
-  // `reducerPath` - имя `slice`, куда будут сохранены состояние и экшены для этого `API`
-  reducerPath: "todolistsApi",
-  tagTypes: ["Todolist"],
-  // `baseQuery` - конфигурация для `HTTP-клиента`, который будет использоваться для отправки запросов
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    prepareHeaders: (headers) => {
-      headers.set("API-KEY", import.meta.env.VITE_API_KEY)
-      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
-    },
-  }),
-  // `endpoints` - метод, возвращающий объект с эндпоинтами для `API`, описанными
-  // с помощью функций, которые будут вызываться при вызове соответствующих методов `API`
-  // (например `get`, `post`, `put`, `patch`, `delete`)
+export const todolistsApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     // Типизация аргументов (<возвращаемый тип, тип query аргументов (`QueryArg`)>)
     // `query` по умолчанию создает запрос `get` и указание метода необязательно
